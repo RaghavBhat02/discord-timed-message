99854e71823bcf980726973f04edf506b185d725
RaghavBhat02 <bhatr@umich.edu>
2023-04-30 01:27:29 -0400

    another server for testing.

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
 
 export const clientId =  "1100898467414540348";
-export const guildId = "1100901718973239427";
+export const guildId =  "1095174633134768321"; //jeff's server
+// export const guildId = "1100901718973239427"; //personal server
